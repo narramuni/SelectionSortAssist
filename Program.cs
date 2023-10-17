d162a5faf92e3098f04d5a30cd64eb3bd9ad956d
narramuni <nmspavan@gmail.com>
2023-10-17 21:42:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSortAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        static void runApp()
+        {
+            string[] students = new string[5];
+
+            students[0] = "SaiPavan";
+            students[1] = "Bhanu";
+            students[2] = "Adhithya";
+            students[3] = "Varun";
+            students[4] = "Guru";
+
+            SelectionSort selectionSort = new SelectionSort();
+            selectionSort.Sort(students);
+
+
+            Console.WriteLine("Sorted List of Students:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student}");
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSortAssist
+{
+    public class SelectionSort
+    {
+        public void Sort(string[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    // Compare student names
+                    if (string.Compare(arr[j], arr[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                // Swap the students
+                string temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
